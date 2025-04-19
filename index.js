595b8fb9027b17bed17a0cb9e4cc7b20034ee8b7
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-04-19 22:12:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+// if handle your project file system use this
+app.use(express.static("public"));
+
+const localhostPort1 = 5173;
+const localhostPort2 = 5174;
+const localhostPort3 = 3000;
+const allowedOrigins = [
+  `http://localhost:${localhostPort1}`,
+  `http://localhost:${localhostPort2}`,
+  `http://localhost:${localhostPort3}`,
+  // here all acces domain name
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  return res.send(" <h1>Welcome to the TOJO News Server</h1>");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running where http://localhost:${PORT}`);
+});
