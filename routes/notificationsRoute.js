0a39ad2d7d097e90dea87c48ae725392fafe47b6
Momen <touristmomen@gmail.com>
2025-06-13 11:42:18 +0600

    notification bell

--- routes/notificationsRoute.js
+++ routes/notificationsRoute.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { getLatestAdminNotifications, markAllNotificationsRead, markNotificationRead } from "../controllers/notificationsController.js";
+import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 const notificationsRoute = express.Router();
 
-notificationsRoute.get("/admin/notifications", getLatestAdminNotifications);
-notificationsRoute.put("/admin/notifications/:id/read", markNotificationRead);
+notificationsRoute.get(
+  "/admin/notifications",
+  verifyAdmin,
+  getLatestAdminNotifications
+);
+notificationsRoute.put(
+  "/admin/notifications/:id/read",
+  verifyAdmin,
+  markNotificationRead
+);
 notificationsRoute.put(
   "/admin/notifications/mark-all-read",
+  verifyAdmin,
   markAllNotificationsRead
 );
 
